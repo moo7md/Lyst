fbb6e52e175750e34bd4f100182b7280d73c849b
moo7md <moo7md3@gmail.com>
2019-11-22 23:14:10 -0500

    added some classes

--- app/src/main/java/com/example/lyst/ChecklistItem.java
+++ app/src/main/java/com/example/lyst/ChecklistItem.java
@@ -2,4 +2,57 @@ package com.example.lyst;
 
 
 public class ChecklistItem {
+    private String id;
+    private String title;
+    private String desc;
+    private String attachmentType;
+    private String attachment;
+
+    public ChecklistItem(String id, String title, String desc, String attachmentType, String attachment) {
+        this.id = id;
+        this.title = title;
+        this.desc = desc;
+        this.attachmentType = attachmentType;
+        this.attachment = attachment;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public String getAttachmentType() {
+        return attachmentType;
+    }
+
+    public String getAttachment() {
+        return attachment;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public void setAttachmentType(String attachmentType) {
+        this.attachmentType = attachmentType;
+    }
+
+    public void setAttachment(String attachment) {
+        this.attachment = attachment;
+    }
 }
---
+++ app/src/main/java/com/example/lyst/Database.java
@@ -0,0 +1,63 @@
+package com.example.lyst;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.ArrayList;
+
+import androidx.annotation.NonNull;
+
+public class Database {
+
+    private FirebaseAuth auth;
+    private FirebaseFirestore db;
+
+    public Database database;
+
+    private Database() {
+        auth = FirebaseAuth.getInstance();
+        db   = FirebaseFirestore.getInstance();
+    }
+
+    public Database getInstance() {
+        if (database == null) {
+            database = new Database();
+        }
+        return this.database;
+    }
+
+    /**
+     * This function checks whether the current user is logged in or not
+     * @return true if user is logged in and false if not.
+     */
+    public boolean isLoggedIn() {
+        return auth.getCurrentUser() == null;
+    }
+
+    /**
+     * Allows the user to login to the app using email and password
+     * @param email email of the user
+     * @param password password associated with the email
+     * @return true if user logged in successfully false if login failed.
+     */
+    public boolean login(String email, String password) {
+        if (!isLoggedIn()) {
+            return auth.signInWithEmailAndPassword(email, password).isSuccessful();
+        }
+        return true;
+    }
+
+    /**
+     * Gives back the user who is currently logged in.
+     * @return null if user not logged in, otherwise an instance of the user 'FirebaseUser'
+     */
+    public FirebaseUser getCurrentUser() {
+        return auth.getCurrentUser();
+    }
+
+    public ArrayList<String> getChecklistItemsFromUser() {
+
+    }
+}
