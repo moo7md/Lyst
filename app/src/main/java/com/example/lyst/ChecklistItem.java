82dcaed1de71f99c75451c7033f824f7a6f1b2f9
aqeelat <abdullah.t.aqeel@gmail.com>
2019-11-22 21:45:51 -0500

    Created part of the front end skeleton

---
+++ app/src/main/java/com/example/lyst/ChecklistItem.java
@@ -0,0 +1,5 @@
+package com.example.lyst;
+
+
+public class ChecklistItem {
+}
---
+++ app/src/main/java/com/example/lyst/LoggedInUserActivity.java
@@ -0,0 +1,14 @@
+package com.example.lyst;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoggedInUserActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_logged_in_user);
+    }
+}
